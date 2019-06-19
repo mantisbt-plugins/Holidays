29535aaa7b8009afde8b8308b3ba46ea313f5dd1
c2pil <c2pil@gmail.com>
2019-06-19 15:41:45 +0200

    Fix sonar issues

--- Holidays.php
+++ Holidays.php
@@ -1,116 +1,177 @@
 <?php
-require_once __DIR__.'/api/utils/utils.php';
+require_once __DIR__ . '/api/utils/utils.php';
+define ( HOLIDAYS_PERIOD, 'period' );
+define ( HOLIDAYS_ABSENT, 'absent' );
 
+/**
+ * Main class of plugin Holidays for Mantis
+ *
+ * @author c2pil
+ *
+ */
 class HolidaysPlugin extends MantisPlugin {
-    
-    
-	function register() {
-		$this->name        = 'Holidays';
-		$this->description = plugin_lang_get( 'description' );
-		$this->version     = '2.21';
-		$this->requires    = array('MantisCore'       => '2.21.0',);
-		$this->author      = array('Cas Nuy', 'c2pil');
-		$this->contact     = 'c2pil@gmail.com';
-	}
 
-	function init() { 
-	
-		plugin_event_hook('EVENT_ACCOUNT_PREF_UPDATE_FORM', 'DefHoliday');
-		plugin_event_hook('EVENT_ACCOUNT_PREF_UPDATE', 'UpdateHoliday');
-		plugin_event_hook('EVENT_MANAGE_USER_DELETE', 'DelHoliday');
-		plugin_event_hook('EVENT_NOTIFY_USER_EXCLUDE', 'MailHoliday');
-		
-	}
-	
-	function config() {
-	    return array(
-	        'date_format' => 'Y-m-d H:i:s'
-	    );
-	}
+    /**
+     * Sets informations of plugin Holidays
+     */
+    public function register() {
+        $this->name = 'Holidays';
+        $this->description = plugin_lang_get ( 'description' );
+        $this->version = '2.21';
+        $this->requires = array (
+                'MantisCore' => '2.21.0'
+        );
+        // Plugin put from Mantis issue to GitHub
+        $this->author = array (
+                'Cas Nuy',
+                'c2pil'
+        );
+        $this->contact = 'c2pil@gmail.com';
+    }
 
-	function DefHoliday(){
-		include( config_get( 'plugin_path' ) . 'Holidays' . DIRECTORY_SEPARATOR . 'pages' . DIRECTORY_SEPARATOR . 'holiday_form.php');  
-	}
-	
-	function MailHoliday($p_event, $p_bug_id, $p_notification_type, $p_user_id ){
-		// get the handler absence status
-		$hol_table	= plugin_table('period','Holidays');
-		$sql 		=  "select * from $hol_table where user_id=$p_user_id";
-		$result	    = db_query($sql);
-		$remove     = false;
-		// check if this person is on holiday		
-		if (db_num_rows($result) > 0) {
-			$row = db_fetch_array($result);
-			// first check absent indicator
-			if ($row['absent'] >0 ){
-				// now check if today is within period defined
-				$today  = mktime(0, 0, 0, date("m")  , date("d"), date("Y"));
-				if (($today>= $row['periodfrom']) and ($today <= $row['periodto']) or ( $row['absent'] == 2 ) ){
-					// Remove the absent user form notification
-					$remove = true;
- 				}
-			}
-		}
-		return $remove;
-	}
-	
-	function UpdateHoliday(){
-	    $user_id    = $_REQUEST['user_id'];
-	    $hol_table	= plugin_table('period','Holidays');
-	    $from		= strtotime(substr($_REQUEST['holidays_start_date'],0,10));
-	    $to			= strtotime(substr($_REQUEST['holidays_end_date'],0,10));
-	    $absent		= $_REQUEST['absent'];
-	    // check on valid absence date
-        if ($absent == 1 and ($from == '' or $to == '' or $from > $to)){
-            trigger_error( 'ERROR_INVALID_DATE', ERROR );
+    /**
+     * Hooks functions to events
+     */
+    public function init() {
+        // Add Absence definition to preference form
+        plugin_event_hook ( 'EVENT_ACCOUNT_PREF_UPDATE_FORM', 'def_holiday' );
+        // Handle absence when updating preferences
+        plugin_event_hook ( 'EVENT_ACCOUNT_PREF_UPDATE', 'update_holiday' );
+        // Handle absence on user deletion
+        plugin_event_hook ( 'EVENT_MANAGE_USER_DELETE', 'delete_holiday' );
+        // Handle notification for absent user
+        plugin_event_hook ( 'EVENT_NOTIFY_USER_EXCLUDE', 'is_user_absent' );
+    }
+
+    /**
+     * Returns the configuration for this plugin
+     *
+     * @return string[]
+     */
+    public function config() {
+        return array (
+                'date_format' => 'Y-m-d H:i:s'
+        );
+    }
+
+    /**
+     * Includes absence field in preferences form
+     */
+    public function def_holiday() {
+        return include_once (plugin_page ( 'holiday_form.php' ));
+    }
+
+    /**
+     * Removes a user from mail notification if he is absent
+     *
+     * @param unknown $p_event
+     * @param unknown $p_bug_id
+     * @param unknown $p_notification_type
+     * @param unknown $p_user_id
+     * @return boolean
+     */
+    public function is_user_absent($p_event, $p_bug_id, $p_notification_type, $p_user_id) {
+        // get the handler absence status
+        $hol_table = plugin_table ( HOLIDAYS_PERIOD );
+        $sql = "select * from $hol_table where user_id=$p_user_id";
+        $result = db_query ( $sql );
+        $remove = false;
+        // check if this person is on holiday
+        if (db_num_rows ( $result ) > 0) {
+            $row = db_fetch_array ( $result );
+            // first check absent indicator
+            if ($row [HOLIDAYS_ABSENT] > 0) {
+                // now check if today is within period defined
+                $today = mktime ( 0, 0, 0, date ( "m" ), date ( "d" ), date ( "Y" ) );
+                if (($today >= $row ['periodfrom']) && ($today <= $row ['periodto']) || ($row [HOLIDAYS_ABSENT] == 2)) {
+                    // Remove the absent user form notification
+                    $remove = true;
+                }
+            }
         }
-	    
-	    // perform update
-	    if ($absent == 1){
-	        $sql = "UPDATE $hol_table set periodfrom=$from, periodto=$to, absent=$absent  WHERE user_id = $user_id";
-	    } else {
-	        $sql = "UPDATE $hol_table set absent=$absent WHERE user_id = $user_id";
-	    }
-	    $result = db_query($sql);
-	    
-	    $auth_username = user_get_name(auth_get_current_user_id());
-	    $updated_username = user_get_name($user_id);
-	    $log_from = substr($_REQUEST['holidays_start_date'],0,10);
-	    $log_to = substr($_REQUEST['holidays_end_date'],0,10);
-	    
-	    switch($absent){
-	        case 0:
-	            $message_log = sprintf( plugin_lang_get('log_present'),
-	                           $updated_username, $auth_username);
-	            break;
-	        case 1:
-	            $message_log = sprintf( plugin_lang_get('log_absent_period'),
-	                           $updated_username, $log_from, $log_to, $auth_username);
-	            break;
-	        case 2:
-	            $message_log = sprintf( plugin_lang_get('log_absent_indefinite'),
-	                           $updated_username, $auth_username);
-	            break;
-	    }
-	    
-	    write_log($message_log);
-	}
-	
-	function DelHoliday($p_event,$f_user_id){
- 		$hol_table	= plugin_table('period');
-		$sql = "delete from $hol_table where user_id=$f_user_id";
-		$result		= db_query($sql);
-	}
+        return $remove;
+    }
 
-	function schema() {
-		return array(
-			array( 'CreateTableSQL', array( plugin_table( 'period' ), "
-						user_id 			I       NOTNULL UNSIGNED PRIMARY,
-						periodfrom			I		,
-						periodto			I		,
-						absent				I		
-						" ) ),
-		);
-	} 
-	
+    /**
+     * Update user status on preferences update
+     */
+    public function update_holiday() {
+
+        // Get params from request
+        $user_id = $_REQUEST ['user_id'];
+        $hol_table = plugin_table ( HOLIDAYS_PERIOD );
+        $from = strtotime ( substr ( $_REQUEST ['holidays_start_date'], 0, 10 ) );
+        $to = strtotime ( substr ( $_REQUEST ['holidays_end_date'], 0, 10 ) );
+        $absent = $_REQUEST [HOLIDAYS_ABSENT];
+        // check on valid absence date
+        if ($absent == 1 && ($from == '' || $to == '' || $from > $to)) {
+            trigger_error ( 'ERROR_INVALID_DATE', ERROR );
+        }
+
+        // perform update
+        if ($absent == 1) {
+            $sql = "UPDATE $hol_table set periodfrom=$from, periodto=$to, absent=$absent WHERE user_id=$user_id";
+        } else {
+            $sql = "UPDATE $hol_table set absent=$absent WHERE user_id= $user_id";
+        }
+        db_query ( $sql );
+
+        $auth_username = user_get_name ( auth_get_current_user_id () );
+        $updated_username = user_get_name ( $user_id );
+        $log_from = substr ( $_REQUEST ['holidays_start_date'], 0, 10 );
+        $log_to = substr ( $_REQUEST ['holidays_end_date'], 0, 10 );
+
+        // Build log message
+        switch ($absent) {
+            case 0 :
+                $message_log = sprintf ( plugin_lang_get ( 'log_present' ), $updated_username, $auth_username );
+                break;
+            case 1 :
+                $message_log = sprintf ( plugin_lang_get ( 'log_absent_period' ),
+                                         $updated_username, $log_from, $log_to, $auth_username );
+                break;
+            case 2 :
+                $message_log = sprintf ( plugin_lang_get ( 'log_absent_indefinite' ),
+                                         $updated_username, $auth_username );
+                break;
+            default :
+                break;
+        }
+        // Log event in log file
+        write_log ( $message_log );
+    }
+
+    /**
+     * Delete user entry in plugin table if the user is deleted
+     *
+     * @param unknown $p_event
+     * @param unknown $f_user_id
+     */
+    public function delete_holiday($p_event, $f_user_id) {
+        $hol_table = plugin_table ( HOLIDAYS_PERIOD );
+        $sql = "delete from $hol_table where user_id=$f_user_id";
+        db_query ( $sql );
+    }
+
+    /**
+     * Table definition on plugin installation
+     *
+     * @return
+     */
+    public function schema() {
+        return array (
+                array (
+                        'CreateTableSQL',
+                        array (
+                                plugin_table ( HOLIDAYS_PERIOD ),
+                                "
+                        user_id             I       NOTNULL UNSIGNED PRIMARY,
+                        periodfrom          I       ,
+                        periodto            I       ,
+                        absent              I
+                        "
+                        )
+                )
+        );
+    }
 }
--- pages/holiday_form.php
+++ pages/holiday_form.php
@@ -1,71 +1,78 @@
 <?PHP
-$file = $_SERVER["SCRIPT_NAME"];
-$break = Explode('/', $file);
-$script = $break[count($break) - 1];
-if (strtolower($script) == "account_prefs_page.php"){
-	$user_id = auth_get_current_user_id();
+// Split URL to get current page file name
+$file = $_SERVER ["SCRIPT_NAME"];
+$break = Explode ( '/', $file );
+$script = $break [count ( $break ) - 1];
+// Get logged in user if in his preference page
+if (strtolower ( $script ) == "account_prefs_page.php") {
+    $user_id = auth_get_current_user_id ();
 } else {
-	$user_id = gpc_get_int( 'user_id' );
+    // Get user_id from request otherwise
+    $user_id = gpc_get_int ( 'user_id' );
 }
 
-// retrieve current available settings
-$hol_table	= plugin_table('period','Holidays');
-$sql 	=  "select * from $hol_table where user_id=$user_id";
-$result	= db_query($sql);
-$count	= db_num_rows($result) ;
-// if not there create with blanks
-if ($count == 0){
-    $sql2 		= "insert into $hol_table values ($user_id, 0,0,0,0)";
-    $result2	= db_query($sql2);
-    $result		= db_query($sql);
+// Retrieve current available settings
+$hol_table = plugin_table ( 'period', 'Holidays' );
+// Get all info from plugin table
+$sql = "select * from $hol_table where user_id=$user_id";
+$result = db_query ( $sql );
+$count = db_num_rows ( $result );
+// If not there create with blanks
+if ($count == 0) {
+    $sql2 = "insert into $hol_table values ($user_id, 0,0,0,0)";
+    db_query ( $sql2 );
+    // Get new result
+    $result = db_query ( $sql );
 }
-while ($row = db_fetch_array($result)) {
-    $from	= $row['periodfrom'] ;
-    $to		= $row['periodto'] ;
-    $absent	= $row['absent'];
+// Get data from results
+while ( $row = db_fetch_array ( $result ) ) {
+    $from = $row ['periodfrom'];
+    $to = $row ['periodto'];
+    $absent = $row ['absent'];
 }
-if ( $from == '0' ) {
+
+// Format dates for display purpose
+if ($from == '0') {
     $from = '';
 } else {
-    $from 	= date( config_get( 'short_date_format' ), $from);
+    $from = date ( config_get ( 'short_date_format' ), $from );
 }
-if ( $to == '0' ) {
+if ($to == '0') {
     $to = '';
 } else {
-    $to	= date( config_get( 'short_date_format' ), $to);
+    $to = date ( config_get ( 'short_date_format' ), $to );
 }
-
 ?>
 <div align="center">
-<input type="hidden" name="user_id" value="<?php echo $user_id ?>" />
-<input type="hidden" name="script" value="<?php echo $script ?>" />
-<!-- Titles -->
-<tr valign="top">
-<td class="category" >
-<?php echo plugin_lang_get( 'absent' ) ?>
-</td>
-<td>
-<label><input type="radio" name='absent' value="2" <?php echo(($absent==2)?'checked=checked':'');?>/>
-<?php echo plugin_lang_get( 'holidays_anyway' )?></label>
-
-<label><input type="radio" name='absent' value="1" <?php echo(($absent==1)?'checked=checked':'');?>/>
-<?php echo plugin_lang_get( 'holidays_enabled' )?></label>
-
-<label><input type="radio" name='absent' value="0" <?php echo(($absent==0)?'checked=checked':'');?>/>
-<?php echo plugin_lang_get( 'holidays_disabled' )?></label>
-<br />
-<label><?php echo plugin_lang_get( 'start_date' ) ?></label>
-<input type="text" id="holidays_start_date" name="holidays_start_date" class="datetimepicker input-sm" size="16" maxlength="16"
-		data-picker-locale="<?php lang_get_current_datetime_locale() ?>"
-		data-picker-format="<?php echo config_get( 'datetime_picker_format' ) ?>"
-		<?php helper_get_tab_index() ?> value="<?php echo $from ?>" />
-	<i class="fa fa-calendar fa-xlg datetimepicker"></i>
-
-<label><?php echo plugin_lang_get( 'end_date' ) ?></label>
-<input type="text" id="holidays_end_date" name="holidays_end_date" class="datetimepicker input-sm" size="16" maxlength="16"
-		data-picker-locale="<?php lang_get_current_datetime_locale() ?>"
-		data-picker-format="<?php echo config_get( 'datetime_picker_format' ) ?>"
-		<?php helper_get_tab_index() ?> value="<?php echo $to ?>" />
-	<i class="fa fa-calendar fa-xlg datetimepicker"></i>
-
-</td>
+    <input type="hidden" name="user_id" value="<?php echo $user_id ?>" />
+    <input type="hidden" name="script" value="<?php echo $script ?>" />
+    <!-- Titles -->
+    <tr valign="top">
+        <td class="category">
+            <?php echo plugin_lang_get( 'absent' ) ?>
+        </td>
+        <td><label> <input type="radio" name='absent' value="2"
+                <?php check_checked( 2, $absent, false );?> />
+                <?php echo plugin_lang_get( 'holidays_anyway' )?>
+            </label> <label> <input type="radio" name='absent' value="1"
+                <?php check_checked( 1, $absent, false );?> />
+                <?php echo plugin_lang_get( 'holidays_enabled' )?>
+            </label> <label> <input type="radio" name='absent' value="0"
+                <?php check_checked( 0, $absent, false );?> />
+                <?php echo plugin_lang_get( 'holidays_disabled' )?>
+            </label> <br /> <label><?php echo plugin_lang_get( 'start_date' ) ?></label>
+            <input type="text" id="holidays_start_date"
+            name="holidays_start_date" class="datetimepicker input-sm"
+            size="16" maxlength="16"
+            data-picker-locale="<?php lang_get_current_datetime_locale() ?>"
+            data-picker-format="<?php echo config_get( 'datetime_picker_format' ) ?>"
+            <?php helper_get_tab_index() ?> value="<?php echo $from ?>" />
+            <i class="fa fa-calendar fa-xlg datetimepicker"></i>
+            <label><?php echo plugin_lang_get( 'end_date' ) ?></label>
+            <input type="text" id="holidays_end_date"
+            name="holidays_end_date" class="datetimepicker input-sm"
+            size="16" maxlength="16"
+            data-picker-locale="<?php lang_get_current_datetime_locale() ?>"
+            data-picker-format="<?php echo config_get( 'datetime_picker_format' ) ?>"
+            <?php helper_get_tab_index() ?> value="<?php echo $to ?>" />
+            <i class="fa fa-calendar fa-xlg datetimepicker"></i></td>
